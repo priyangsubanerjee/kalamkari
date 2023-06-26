d3b5455414b215f035db5882eca8d414ea2d213d
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-26 20:10:50 +0530

    auth state

--- pages/inventory.js
+++ pages/inventory.js
@@ -23,38 +23,79 @@ function Dashboard() {
   return (
     <div>
       <Navbar />
-      <div className="px-0 lg:px-24 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 lg:gap-5 lg:mt-10">
-        <div className="border-b border-r lg:border">
-          <div className="h-36 bg-yellow-100 relative overflow-hidden">
-            <img
-              src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/22536178/2023/3/28/060630ba-0cb0-46f5-ab19-59a0d7502af41679989291511KALINIMaroonBlueWovenDesignZariPureSilkKanjeevaramSaree1.jpg"
-              alt=""
-              className="absolute inset-0 w-full"
-            />
-          </div>
-          <div className="p-3">
-            <h1 className="text-base font-bold">₹9000</h1>
-            <h2 className="text-sm mt-1">Sarees (साड़ी)</h2>
-            <div className="mt-4 grid grid-cols-3 text-xs">
-              <div className="flex items-center">
-                <span className="text-blue-600 mt-1">
-                  <iconify-icon icon="bi:box"></iconify-icon>
-                </span>
-                <span className="ml-2 text-sm">10</span>
+      <>
+        {user !== null && user !== false && (
+          <>
+            <div className="px-0 lg:px-24 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 lg:gap-5 lg:mt-10">
+              <div className="border-b border-r lg:border">
+                <div className="h-36 bg-yellow-100 relative overflow-hidden">
+                  <img
+                    src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/22536178/2023/3/28/060630ba-0cb0-46f5-ab19-59a0d7502af41679989291511KALINIMaroonBlueWovenDesignZariPureSilkKanjeevaramSaree1.jpg"
+                    alt=""
+                    className="absolute inset-0 w-full"
+                  />
+                </div>
+                <div className="p-3">
+                  <h1 className="text-base font-bold">₹9000</h1>
+                  <h2 className="text-sm mt-1">Sarees (साड़ी)</h2>
+                  <div className="mt-4 grid grid-cols-3 text-xs">
+                    <div className="flex items-center">
+                      <span className="text-blue-600 mt-1">
+                        <iconify-icon icon="bi:box"></iconify-icon>
+                      </span>
+                      <span className="ml-2 text-sm">10</span>
+                    </div>
+                    <div className="flex items-center">
+                      <span className="text-sm text-red-700">₹4000</span>
+                    </div>
+                    <div className="flex items-center">
+                      <span className="text-green-600">
+                        <iconify-icon icon="bi:caret-up-fill"></iconify-icon>
+                      </span>
+                      <span className="ml-2 text-sm">33%</span>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="flex items-center">
-                <span className="text-sm text-red-700">₹4000</span>
-              </div>
-              <div className="flex items-center">
-                <span className="text-green-600">
-                  <iconify-icon icon="bi:caret-up-fill"></iconify-icon>
-                </span>
-                <span className="ml-2 text-sm">33%</span>
+              <div className="border-b border-r lg:border">
+                <div className="h-36 bg-yellow-100 relative overflow-hidden">
+                  <img
+                    src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/22536178/2023/3/28/060630ba-0cb0-46f5-ab19-59a0d7502af41679989291511KALINIMaroonBlueWovenDesignZariPureSilkKanjeevaramSaree1.jpg"
+                    alt=""
+                    className="absolute inset-0 w-full"
+                  />
+                </div>
+                <div className="p-3">
+                  <h1 className="text-base font-bold">₹9000</h1>
+                  <h2 className="text-sm mt-1">Sarees (साड़ी)</h2>
+                  <div className="mt-4 grid grid-cols-3 text-xs">
+                    <div className="flex items-center">
+                      <span className="text-blue-600 mt-1">
+                        <iconify-icon icon="bi:box"></iconify-icon>
+                      </span>
+                      <span className="ml-2 text-sm">10</span>
+                    </div>
+                    <div className="flex items-center">
+                      <span className="text-sm text-red-700">₹4000</span>
+                    </div>
+                    <div className="flex items-center">
+                      <span className="text-green-600">
+                        <iconify-icon icon="bi:caret-up-fill"></iconify-icon>
+                      </span>
+                      <span className="ml-2 text-sm">33%</span>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
+            <div className="fixed bottom-0 inset-x-0 p-4 border-t bg-neutral-50">
+              <button className="w-full h-14 bg-black rounded-md text-white">
+                Add product
+              </button>
+            </div>
+          </>
+        )}
+      </>
     </div>
   );
 }
