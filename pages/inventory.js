d7c5339b7cef5e461154ef4c6c60e53d990c6126
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-26 19:20:08 +0530

    product

--- pages/inventory.js
+++ pages/inventory.js
@@ -22,91 +22,24 @@ function Dashboard() {
   return (
     <div>
       <Navbar />
-      <div className="px-0 lg:px-24 grid grid-cols-2 lg:grid-cols-4">
-        <div className="border">
-          <div className="h-44 bg-sky-100"></div>
-          <div className="p-3">
-            <h1 className="text-lg font-semibold">₹9000</h1>
-            <h2 className="text-sm mt-1">Sarees (साड़ी)</h2>
-          </div>
-          <div className="mt-2 grid grid-cols-2">
-            <button className="px-5 py-2 bg-neutral-100 w-full">
-              <iconify-icon
-                height="20"
-                width="20"
-                icon="fluent:edit-20-regular"
-              ></iconify-icon>
-            </button>
-            <button className="px-5 py-2 bg-red-100 w-full">
-              <iconify-icon
-                height="20"
-                width="20"
-                icon="mdi-light:delete"
-              ></iconify-icon>
-            </button>
-          </div>
-        </div>
-        <div className="border">
-          <div className="h-44 bg-fuchsia-100"></div>
-          <div className="p-3">
-            <h1 className="text-lg font-semibold">₹9000</h1>
-            <h2 className="text-sm mt-1">Sarees (साड़ी)</h2>
-          </div>
-          <div className="mt-2 grid grid-cols-2">
-            <button className="px-5 py-2 bg-neutral-100 w-full">
-              <iconify-icon
-                height="20"
-                width="20"
-                icon="fluent:edit-20-regular"
-              ></iconify-icon>
-            </button>
-            <button className="px-5 py-2 bg-red-100 w-full">
-              <iconify-icon
-                height="20"
-                width="20"
-                icon="mdi-light:delete"
-              ></iconify-icon>
-            </button>
-          </div>
-        </div>{" "}
-        <div className="border">
+      <div className="px-0 lg:px-24 grid grid-cols-2 lg:grid-cols-4 divide-x">
+        <div className="border-b">
           <div className="h-44 bg-yellow-100"></div>
           <div className="p-3">
-            <h1 className="text-lg font-semibold">₹9000</h1>
+            <h1 className="text-base font-bold">₹9000</h1>
             <h2 className="text-sm mt-1">Sarees (साड़ी)</h2>
           </div>
           <div className="mt-2 grid grid-cols-2">
-            <button className="px-5 py-2 bg-neutral-100 w-full">
-              <iconify-icon
-                height="20"
-                width="20"
-                icon="fluent:edit-20-regular"
-              ></iconify-icon>
-            </button>
-            <button className="px-5 py-2 bg-red-100 w-full">
-              <iconify-icon
-                height="20"
-                width="20"
-                icon="mdi-light:delete"
-              ></iconify-icon>
-            </button>
-          </div>
-        </div>{" "}
-        <div className="border">
-          <div className="h-44 bg-green-100"></div>
-          <div className="p-3">
-            <h1 className="text-lg font-semibold">₹9000</h1>
-            <h2 className="text-sm mt-1">Sarees (साड़ी)</h2>
-          </div>
-          <div className="mt-2 grid grid-cols-2">
-            <button className="px-5 py-2 bg-neutral-100 w-full">
-              <iconify-icon
-                height="20"
-                width="20"
-                icon="fluent:edit-20-regular"
-              ></iconify-icon>
-            </button>
-            <button className="px-5 py-2 bg-red-100 w-full">
+            <div className="flex items-center justify-center">
+              <button className="px-5 py-1 rounded-full bg-neutral-100 w-fit">
+                <iconify-icon
+                  height="20"
+                  width="20"
+                  icon="fluent:edit-20-regular"
+                ></iconify-icon>
+              </button>
+            </div>
+            <button className="px-5 py-3 bg-red-100 w-full">
               <iconify-icon
                 height="20"
                 width="20"
