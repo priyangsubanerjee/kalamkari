7b60fd1cdd8595e77edd4944d5bff22384a098ce
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-26 19:36:14 +0530

    demo products

--- pages/inventory.js
+++ pages/inventory.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/exhaustive-deps */
 import Navbar from "@/components/Navbar";
 import GlobalStates from "@/context/GlobalStateContext";
@@ -22,16 +23,22 @@ function Dashboard() {
   return (
     <div>
       <Navbar />
-      <div className="px-0 lg:px-24 grid grid-cols-2 lg:grid-cols-4 divide-x">
-        <div className="border-b">
-          <div className="h-44 bg-yellow-100"></div>
+      <div className="px-0 lg:px-24 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 lg:gap-5 lg:mt-10">
+        <div className="border-b border-r lg:border">
+          <div className="h-44 bg-yellow-100 relative overflow-hidden">
+            <img
+              src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/22536178/2023/3/28/060630ba-0cb0-46f5-ab19-59a0d7502af41679989291511KALINIMaroonBlueWovenDesignZariPureSilkKanjeevaramSaree1.jpg"
+              alt=""
+              className="absolute inset-0 w-full"
+            />
+          </div>
           <div className="p-3">
             <h1 className="text-base font-bold">₹9000</h1>
             <h2 className="text-sm mt-1">Sarees (साड़ी)</h2>
           </div>
           <div className="grid grid-cols-2 py-2">
             <div className="flex items-center justify-center">
-              <button className="px-5 pt-1 rounded-full bg-neutral-100 w-[80%]">
+              <button className="px-5 pt-1 rounded-full bg-neutral-100 w-[90%]">
                 <iconify-icon
                   height="20"
                   width="20"
@@ -40,7 +47,40 @@ function Dashboard() {
               </button>
             </div>
             <div className="flex items-center justify-center">
-              <button className="px-5 pt-1 rounded-full bg-red-100 w-[80%]">
+              <button className="px-5 pt-1 rounded-full bg-red-100 w-[90%]">
+                <iconify-icon
+                  height="20"
+                  width="20"
+                  icon="mdi-light:delete"
+                ></iconify-icon>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="border-b border-r lg:border">
+          <div className="h-44 bg-yellow-100 relative overflow-hidden">
+            <img
+              src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/22536178/2023/3/28/060630ba-0cb0-46f5-ab19-59a0d7502af41679989291511KALINIMaroonBlueWovenDesignZariPureSilkKanjeevaramSaree1.jpg"
+              alt=""
+              className="absolute inset-0 w-full"
+            />
+          </div>
+          <div className="p-3">
+            <h1 className="text-base font-bold">₹9000</h1>
+            <h2 className="text-sm mt-1">Sarees (साड़ी)</h2>
+          </div>
+          <div className="grid grid-cols-2 py-2">
+            <div className="flex items-center justify-center">
+              <button className="px-5 pt-1 rounded-full bg-neutral-100 w-[90%]">
+                <iconify-icon
+                  height="20"
+                  width="20"
+                  icon="fluent:edit-20-regular"
+                ></iconify-icon>
+              </button>
+            </div>
+            <div className="flex items-center justify-center">
+              <button className="px-5 pt-1 rounded-full bg-red-100 w-[90%]">
                 <iconify-icon
                   height="20"
                   width="20"
