c1841177010928cefd13096bda39e40865fc3ac0
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-26 19:59:23 +0530

    modular components

--- pages/inventory.js
+++ pages/inventory.js
@@ -35,32 +35,22 @@ function Dashboard() {
           <div className="p-3">
             <h1 className="text-base font-bold">₹9000</h1>
             <h2 className="text-sm mt-1">Sarees (साड़ी)</h2>
-          </div>
-          <div className="py-2 mb-1">
-            <div className="flex items-center justify-center">
-              <button className="px-5 pt-2 pb-1 rounded-full bg-neutral-100 w-[90%]">
-                <iconify-icon icon="solar:eye-outline"></iconify-icon>
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="border-b border-r lg:border">
-          <div className="h-36 bg-yellow-100 relative overflow-hidden">
-            <img
-              src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/22536178/2023/3/28/060630ba-0cb0-46f5-ab19-59a0d7502af41679989291511KALINIMaroonBlueWovenDesignZariPureSilkKanjeevaramSaree1.jpg"
-              alt=""
-              className="absolute inset-0 w-full"
-            />
-          </div>
-          <div className="p-3">
-            <h1 className="text-base font-bold">₹9000</h1>
-            <h2 className="text-sm mt-1">Sarees (साड़ी)</h2>
-          </div>
-          <div className="py-2 mb-1">
-            <div className="flex items-center justify-center">
-              <button className="px-5 pt-2 pb-1 rounded-full bg-neutral-100 w-[90%]">
-                <iconify-icon icon="solar:eye-outline"></iconify-icon>
-              </button>
+            <div className="mt-4 grid grid-cols-3">
+              <div className="flex items-center">
+                <span className="text-blue-600 mt-1">
+                  <iconify-icon icon="bi:box"></iconify-icon>
+                </span>
+                <span className="ml-2 text-sm">10</span>
+              </div>
+              <div className="flex items-center">
+                <span className="text-sm text-red-700">₹4000</span>
+              </div>
+              <div className="flex items-center">
+                <span className="text-green-500">
+                  <iconify-icon icon="bi:caret-up-fill"></iconify-icon>
+                </span>
+                <span className="ml-2 text-sm">33%</span>
+              </div>
             </div>
           </div>
         </div>
