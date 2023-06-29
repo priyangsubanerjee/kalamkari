620420f09ad6165d9d9024ff9e132931d0ac6f9d
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-06-29 17:49:17 +0530

    auth

--- components/Navbar.js
+++ components/Navbar.js
@@ -40,22 +40,6 @@ function Navbar() {
             })}
           </ul>
         )}
-        <button className="ml-auto">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1"
-            stroke="currentColor"
-            class="w-8 h-8"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-            />
-          </svg>
-        </button>
       </div>
       {authState == "authenticated" && (
         <ul className="lg:hidden text-sm flex items-center space-x-2 px-6 py-4 border-b overflow-auto shrink-0">
